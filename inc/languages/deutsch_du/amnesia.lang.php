8814e7fb92d6a01a965ba4f57e3b3b2967e6dd0a
Thomas131 <t@t131.us.to>
2019-09-29 19:18:52 +0200

    Add informal german translation of non-admin stuff

---
+++ inc/languages/deutsch_du/amnesia.lang.php
@@ -0,0 +1,62 @@
+<?php
+
+$l['amnesia_privacy_policy_agreement_required'] = 'Bitte lies und akzeptiere die Datenschutzerkl&auml;rung bevor du unsere Webseite verwendest.';
+$l['amnesia_privacy_policy_signup_statement'] = 'Beim weitermachen erkl&auml;rst du dich mit der <a href="{1}">Datenschutzerkl&auml;rung</a> einverstanden.';
+$l['amnesia_privacy_policy_accept'] = 'Akzeptieren';
+$l['amnesia_privacy_policy_date'] = 'Zuletzt bearbeitet {1}';
+$l['amnesia_privacy_policy_date_last_read'] = 'Akzeptiert {1}';
+$l['amnesia_privacy_policy_accepted'] = 'Danke, du wirst jetzt zur&uuml;ck geleitet.';
+
+$l['amnesia_personal_data_erasure'] = 'Pers&ouml;nliche Datenl&ouml;schung';
+$l['amnesia_personal_data_erasure_description'] = 'Hier kannst du die L&ouml;schung deiner pers&ouml;hnlichen Daten beantragen. Dieser Schritt wird deinen Account <strong>permanent deaktivieren</strong>. Ein Verifizierungscode wird an deine E-Mail-Adresse gesendet, um die Anfrage zu best&auml;tigen.';
+$l['amnesia_personal_data_erasure_waiting_period_days'] = 'Deine Daten weren nach {1} Tagen gel&ouml;scht. W&auml;hrend dieser Zeit kannst du diesen Vorgang nach dem Login abbrechen.';
+$l['amnesia_personal_data_erasure_approval'] = 'Deine Daten werden gel&ouml;scht, sobald der Administrator deinen Antrag best&auml;tigt.';
+$l['amnesia_personal_data_erasure_options'] = 'L&ouml;schoptionen';
+$l['amnesia_personal_data_erasure_content'] = 'l&ouml;sche geposteten Inhalt';
+$l['amnesia_personal_data_erasure_content_description'] = 'W&auml;hle, ob geposteter Inhalt von deinem Account (Posts, Threads, etc.) auch gel&ouml;scht werden sollen.';
+$l['amnesia_personal_data_erasure_comment'] = 'Kommentar';
+$l['amnesia_personal_data_erasure_comment_description'] = 'Du kannst weitere Details zu deinem Antrag f&uuml;r den Forenadministrator hinf&uuml;gen.';
+$l['amnesia_personal_data_erasure_proceed'] = 'Beantrage L&ouml;schung';
+$l['amnesia_personal_data_erasure_no_super_admin'] = 'Bitte l&ouml;sche deinen Account von der Liste der Super Administratoren, um Datenl&ouml;schung beantragen zu k&ouml;nnen.';
+$l['amnesia_personal_data_erasure_requested'] = 'Ein Verifizierungscode wurde an deine E-Mail-Adresse gesendet.';
+$l['amnesia_personal_data_erasure_request_verification_subject'] = 'Best&auml;tige deinen L&ouml;schantrag';
+$l['amnesia_personal_data_erasure_request_verification_message'] = '{1},
+
+Um den L&ouml;sch-Prozess der pers&ouml;hnlichen Daten bei {2} zu beenden, musst du auf die URL unten im Webbrowser klicken. Der Code ist f&uuml;r 24 Stunden g&uuml;ltig.
+
+{3}
+
+Danke,
+{2} Team';
+$l['amnesia_personal_data_erasure_pending'] = 'Die L&ouml;schung pers&ouml;hnlicher Daten wird ausgef&uuml;hrt';
+$l['amnesia_personal_data_erasure_pending_message'] = 'Du hast die L&ouml;schung perslhnlicher Daten beantragt.';
+$l['amnesia_personal_data_erasure_scheduled_for'] = 'Die L&ouml;schung wird f&uuml;r {1} angesetzt.';
+$l['amnesia_personal_data_erasure_cancel'] = 'L&ouml;schung abbrechen';
+
+$l['amnesia_personal_data_erasure_awaiting_approval_single'] = 'Da ist ein Antrag zur L&ouml;schung pers&ouml;hnlicher Daten, der auf Bewilligung wartet. Bitte geh zu deinem ACP, um den Antrag zu &uuml;berpr&uuml;fen.';
+$l['amnesia_personal_data_erasure_awaiting_approval_plural'] = 'Da sind {1} Antr&auml;ge zur L&ouml;schung pers&ouml;hnlicher Daten, der auf Bewilligung warten. Bitte geh zu deinem ACP, um den Antrag zu &uuml;berpr&uuml;fen.';
+$l['amnesia_personal_data_erasure_awaiting_approval_link'] = ' <a href="{1}">Geh zum ACP</a>.';
+
+
+$l['amnesia_personal_data_export'] = 'Pers&ouml;hnlicher Datenexport';
+$l['amnesia_personal_data_export_description'] = 'Hier kannst du eine Kopie deiner pers&ouml;hnlcihen Daten in einem maschinenenlesbaren XML-Format beantragen. Ein Verifizierungsvode wird dir per E-Mail gesendet, um den Antrag zu best&auml;tigen.';
+$l['amnesia_personal_data_export_key_invalid'] = 'Der Verifizierungslink muss mit exakt dem gleichen Ger&auml;t und der gleichen Browser-Session aufgerufen werden. Bitte beantrage einen neuen Datenexport.';
+$l['amnesia_personal_data_export_proceed'] = 'Fortfahren';
+$l['amnesia_personal_data_export_requested'] = 'Ein Verifizierungscode wurde per E-Mail an dich gesendet.';
+$l['amnesia_personal_data_export_request_verification_subject'] = 'Best&auml;tige den Antrag zum Export deiner pers&ouml;hnlichen Daten';
+$l['amnesia_personal_data_export_request_verification_message'] = '{1},
+
+Um den Exportprozess der pers&ouml;hnlichen Daten bei {2} zu beenden, musst du die URL unten in dem Webbrowser aufrufen, in dem du die Anfrage gestellt hast. Der Code ist f&uuml;r 24 Stunden g&uuml;ltig.
+
+{3}
+
+Danke,
+{2} Team';
+$l['amnesia_personal_data_export_verify'] = 'Du kannst nun eine Datei mit den pers&ouml;hnlichen Daten in Verbindung mit deinem Account herunterladen.';
+$l['amnesia_personal_data_export_download'] = 'Download';
+
+$l['amnesia_verification_code_invalid'] = 'Der Verifizierungscode ist ung&uuml;ltig oder veraltet.';
+
+$l['amnesia_anonymized_username_prefix'] = 'Benutzer ';
+
+$l['amnesia_erasure_task_ran'] = 'Die Amnesia L&ouml;schungs-Aufgabe wurde erfolgreich abgeschlossen.';
